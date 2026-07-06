9de9678a64029fd99b567a12d6e34578bef0fb27
Andrei <duircianos@yandex.ru>
2026-07-06 14:13:41 +0400

    keyboard fix

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,54 +1,57 @@
-import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { KeyboardProvider } from 'react-native-keyboard-controller';
+import React, {useEffect} from 'react';
+import {StatusBar} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {KeyboardProvider} from 'react-native-keyboard-controller';
 
-import { AppLifecycleProvider } from './src/context/AppLifecycleContext';
-import { PostAuthBootstrapManager } from './src/components/PostAuthBootstrapManager';
-import { AuthProvider } from './src/context/AuthContext';
-import { CallProvider } from './src/context/CallContext';
-import { NotificationsProvider } from './src/context/NotificationsContext';
-import { UnreadProvider } from './src/context/UnreadContext';
-import { AppNavigator } from './src/navigation/AppNavigator';
-import { ThemeProvider, useThemeColors } from './src/theme/ThemeContext';
-import { logCallEnvOnce } from './src/utils/callDiagnostics';
+import {AppLifecycleProvider} from './src/context/AppLifecycleContext';
+import {PostAuthBootstrapManager} from './src/components/PostAuthBootstrapManager';
+import {AuthProvider} from './src/context/AuthContext';
+import {CallProvider} from './src/context/CallContext';
+import {NotificationsProvider} from './src/context/NotificationsContext';
+import {UnreadProvider} from './src/context/UnreadContext';
+import {AppNavigator} from './src/navigation/AppNavigator';
+import {ThemeProvider, useThemeColors} from './src/theme/ThemeContext';
+import {logCallEnvOnce} from './src/utils/callDiagnostics';
+import {NavigationContainer} from "@react-navigation/native";
 
 function AppContent() {
-  const colors = useThemeColors();
+    const colors = useThemeColors();
 
-  useEffect(() => {
-    logCallEnvOnce('app_start');
-  }, []);
+    useEffect(() => {
+        logCallEnvOnce('app_start');
+    }, []);
 
-  return (
-    <>
-      <StatusBar
-        barStyle={colors.statusBarStyle}
-        backgroundColor={colors.background}
-      />
-      <AuthProvider>
-        <AppLifecycleProvider>
-          <PostAuthBootstrapManager />
-          <UnreadProvider>
-            <NotificationsProvider>
-              <CallProvider>
-                <AppNavigator />
-              </CallProvider>
-            </NotificationsProvider>
-          </UnreadProvider>
-        </AppLifecycleProvider>
-      </AuthProvider>
-    </>
-  );
+    return (
+        <>
+            <StatusBar
+                barStyle={colors.statusBarStyle}
+                backgroundColor={colors.background}
+            />
+            <AuthProvider>
+                <AppLifecycleProvider>
+                    <PostAuthBootstrapManager/>
+                    <UnreadProvider>
+                        <NotificationsProvider>
+                            <CallProvider>
+                                <AppNavigator/>
+                            </CallProvider>
+                        </NotificationsProvider>
+                    </UnreadProvider>
+                </AppLifecycleProvider>
+            </AuthProvider>
+        </>
+    );
 }
 
 function App() {
     return (
         <SafeAreaProvider>
             <KeyboardProvider statusBarTranslucent navigationBarTranslucent>
-                <ThemeProvider>
-                    <AppContent />
-                </ThemeProvider>
+                <NavigationContainer>
+                    <ThemeProvider>
+                        <AppContent/>
+                    </ThemeProvider>
+                </NavigationContainer>
             </KeyboardProvider>
         </SafeAreaProvider>
     );
